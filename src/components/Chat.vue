b4873343f4bda364c3f972cce5ca5045483d5fa9
Fusuccess <fu2000520@tom.com>
2025-04-28 23:12:36 +0800

    feat(web): 🚀 增加注册入口和关于入口选项

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -1,30 +1,37 @@
 <template>
   <div class="chat-container">
-    <div v-if="loginStatus" class="chat-box">
-    <h1>AI代码注释与问题解答</h1>
-    <textarea v-model="userInput" :maxlength=maxLength :placeholder="`请输入你的问题（最多${maxLength}字符）`"  @input="validateInput"></textarea>
-    <p>剩余字符数：{{ maxLength - userInput.length }}</p>
-    <button @click="sendRequest" :disabled="loading">
-      <span v-if="loading">加载中...</span>
-      <span v-else>发送</span></button>
-    <div v-if="loading">请求中...</div>
-    <div v-if="response">
-      <h2>AI回应：</h2>
-      <div v-if="response" class="ai-response answer" v-html="renderMarkdown(response)"></div>
+    <div class="userInfo" v-if="loginStatus=='logged'">{{ userInfo.username }}</div>
+    <div class="userInfo" v-else><select v-model="loginStatus" @change="changeHomeTab">
+      <option value="login" >登陆</option>
+      <option value="register" >注册</option>
+      <option value="about">关于</option>
+    </select></div>
+    <div v-if="loginStatus=='logged'" class="chat-box">
+      <h1>AI代码注释与问题解答</h1>
+      <textarea v-model="userInput" :maxlength=maxLength :placeholder="`请输入你的问题（最多${maxLength}字符）`"
+                @input="validateInput"></textarea>
+      <p>剩余字符数：{{ maxLength - userInput.length }}</p>
+      <button @click="sendRequest" :disabled="loading">
+        <span v-if="loading">加载中...</span>
+        <span v-else>发送</span></button>
+      <div v-if="loading">请求中...</div>
+      <div v-if="response">
+        <h2>AI回应：</h2>
+        <div v-if="response" class="ai-response answer" v-html="renderMarkdown(response)"></div>
+      </div>
+      <div v-if="error" class="error">{{ error }}</div>
+      <div v-if="history.length > 0">
+        <h3>历史记录</h3>
+        <ul class="answer">
+          <li v-for="(item, index) in history" :key="index">
+            <hr/>
+            <p><span>{{ item.prompt }}</span></p>
+            <p><span v-html="renderMarkdown(item.answer)"></span></p>
+          </li>
+        </ul>
+      </div>
     </div>
-    <div v-if="error" class="error">{{ error }}</div>
-    <div v-if="history.length > 0">
-      <h3>历史记录</h3>
-      <ul class="answer">
-        <li v-for="(item, index) in history" :key="index">
-          <hr/>
-          <p><span>{{ item.prompt }}</span></p>
-          <p><span v-html="renderMarkdown(item.answer)"></span></p>
-        </li>
-      </ul>
-    </div>
-    </div>
-    <div v-else>
+    <div v-else-if="loginStatus=='login'">
       <h1>登陆</h1>
 
       账号：<input placeholder="请输入账号" v-model="user.username">
@@ -37,6 +44,28 @@
         <span v-if="loading">登陆中...</span>
         <span v-else>登陆</span></button>
     </div>
+
+    <div v-else-if="loginStatus=='register'">
+      <h1>注册</h1>
+
+      账号：<input placeholder="请输入账号" v-model="user.username">
+      <br>
+      密码：<input placeholder="请输入密码" type="password" v-model="user.password">
+      <br>
+      密码：<input placeholder="请再次输入密码" type="password" v-model="user.confirmPassword">
+      <br>
+      <div v-if="error" class="error">{{ error }}</div>
+
+      <button @click="registerRequest" :disabled="loading">
+        <span v-if="loading">注册中...</span>
+        <span v-else>注册</span></button>
+    </div>
+
+    <div v-else-if="loginStatus=='about'">
+      <h1>关于</h1>
+      <span v-html="renderMarkdown(about)"></span>
+    </div>
+
   </div>
 </template>
 
@@ -53,14 +82,51 @@ export default {
       loading: false,
       history: [],  // 用来保存历史记录
       maxLength: 200,  // 限制输入字符数
-      loginStatus: false,  // 登录状态
+      loginStatus: 'login',  // 登录状态  logged  login   register
       user: {
         username: '',
-        password: ''
-      }
+        password: '',
+        confirmPassword: ''
+      },
+      userInfo: {
+        username: ''
+      },
+      about:" 👤 Ordinary young programmer, _BSc in Computer Science_\n <br>" +
+          " 🏃 Cycling / swimming / running / badminton\n <br>" +
+          " 🗺️ ShangHai / WuHan / GuangZhou / ShenZhen\n <br>" +
+          " 💻 [Website](https://fusuccess.top)"
     };
   },
   methods: {
+    async registerRequest() {
+      if (this.user.username === '' || this.user.password === '' || this.user.confirmPassword === '') {
+        this.error = '账号或密码不能为空';
+        return;
+      }
+
+      if (this.user.password !== this.user.confirmPassword) {
+        this.error = '两次密码不一致';
+        return;
+      }
+      this.loading = true;
+      const res = await axios.post('http://localhost:8080/auth/register', {
+        username: this.user.username,
+        password: this.user.password
+      });
+      if (res.data && res.data.status === 'success') {
+        console.log(res.data.data)
+        // localStorage.setItem('token', res.data.data);  // 将 token 保存在 localStorage
+        this.loginStatus = 'login';  // 登录成功，更新登录状态
+        // this.userInfo.username = this.user.username;
+        this.error = '注册成功，请登录';
+      } else {
+        this.error = res.data.data;
+      }
+      this.loading = false;
+    },
+    changeHomeTab(){
+      console.log(this.home_tab)
+    },
     async loginRequest() {
       if (this.user.username === '' || this.user.password === '') {
         this.error = '账号或密码不能为空';
@@ -71,12 +137,13 @@ export default {
         username: this.user.username,
         password: this.user.password
       });
-      if (res.data && res.data.status ==='success') {
+      if (res.data && res.data.status === 'success') {
         console.log(res.data.data)
         localStorage.setItem('token', res.data.data);  // 将 token 保存在 localStorage
-        this.loginStatus = true;  // 登录成功，更新登录状态
-      }else {
-        this.error =  res.data.data;
+        this.loginStatus = 'logged';  // 登录成功，更新登录状态
+        this.userInfo.username = this.user.username;
+      } else {
+        this.error = res.data.data;
       }
       this.loading = false;
     },
@@ -101,7 +168,7 @@ export default {
       this.loading = true;
 
       let response = this.localStorageLoad(`aiResponse_${this.userInput}`);
-      if(response !== null){
+      if (response !== null) {
         this.response = response;
         this.history.push({
           prompt: this.userInput,
@@ -111,7 +178,7 @@ export default {
         return;
       }
       try {
-        const res =await axios.post('http://localhost:8080/ai', {
+        const res = await axios.post('http://localhost:8080/ai', {
           prompt: this.userInput
         });
         if (res.data && res.data.status === 'success') {
@@ -120,9 +187,9 @@ export default {
             prompt: this.userInput,
             answer: this.response
           });
-          this.localStorageSave(`aiResponse_${this.userInput}`,this.response ,1800000);
+          this.localStorageSave(`aiResponse_${this.userInput}`, this.response, 1800000);
         } else {
-          this.error =  res.data.data;
+          this.error = res.data.data;
         }
       } catch (err) {
         if (err.response) {
@@ -139,7 +206,7 @@ export default {
         this.loading = false;
       }
     },
-    localStorageSave(key,value, expiryTime = 3600000){
+    localStorageSave(key, value, expiryTime = 3600000) {
       const data = {
         value: value,
         timestamp: Date.now(),  // 当前时间戳
@@ -147,7 +214,7 @@ export default {
       };
       localStorage.setItem(key, JSON.stringify(data));
     },
-    localStorageLoad(key){
+    localStorageLoad(key) {
       const cachedResponse = JSON.parse(localStorage.getItem(key));
       if (!cachedResponse) {
         return null;  // 没有缓存数据
@@ -174,21 +241,31 @@ export default {
   margin: 50px auto;
   text-align: center;
 }
+
 textarea {
   width: 100%;
   height: 100px;
   margin-bottom: 10px;
 }
+
 button {
   padding: 10px 20px;
   margin: 10px 0;
   cursor: pointer;
 }
+
 .error {
   color: red;
   font-size: 12px;
 }
-.answer{
+
+.answer {
   text-align: left;
+}
+
+.userInfo {
+  margin-right: 10px;
+  text-align: right;
+
 }
 </style>
